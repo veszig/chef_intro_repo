73d5224904b0e54f06197370f87fab192d4277ee
Gabor Veszi <veszig@done.hu>
2012-07-20 11:47:35 +0200

    Bonus step: set some other basic apache options and do a little cleanup.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -12,3 +12,24 @@ template "/etc/apache2/conf.d/identity" do
     :serveradmin => node['apache']['serveradmin']
   )
 end
+
+cookbook_file "/etc/apache2/conf.d/security" do
+  owner "root"
+  group "root"
+  mode 0644
+  source "security"
+end
+
+%w(conf.d/security sites-enabled/000-default).each { |f|
+  file "/etc/apache2/#{f}" do
+    action :delete
+  end
+}
+
+file "/var/www/index.html" do
+  owner "root"
+  group "root"
+  mode 0644
+  content "<!-- hello -->\n"
+  only_if "grep -q 'It works' /var/www/index.html"
+end
