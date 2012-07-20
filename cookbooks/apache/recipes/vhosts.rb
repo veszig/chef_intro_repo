65c964c479a53f4c047af5f852b6e9bd303aee62
Gabor Veszi <veszig@done.hu>
2012-07-20 11:46:05 +0200

    Read vhosts from data bags.

--- cookbooks/apache/recipes/vhosts.rb
+++ cookbooks/apache/recipes/vhosts.rb
@@ -1,20 +1,32 @@
-directory "/var/www/foobar.com" do
-  owner "www-data"
-  group "www-data"
-  mode 0755
-end
+data_bag('vhosts').each do |v|
+  vhost = data_bag_item('vhosts', v)
 
-template "/etc/apache2/sites-available/foobar.com" do
-  owner "root"
-  group "root"
-  mode 0644
-  source "vhost.conf.erb"
-  variables(
-    :servername => "foobar.com",
-    :serveralias => "www.foobar.com"
-  )
-end
+  next if vhost['servername'].to_s.empty?
 
-link "/etc/apache2/sites-enabled/foobar.com" do
-  to "/etc/apache2/sites-available/foobar.com"
+  directory "/var/www/#{vhost['servername']}" do
+    owner "www-data"
+    group "www-data"
+    mode 0755
+  end
+
+  if %w(true yes on 1).include?(vhost['default_vhost'].to_s)
+    confname = "000-#{vhost['servername']}"
+  else
+    confname = vhost['servername']
+  end
+
+  template "/etc/apache2/sites-available/#{confname}" do
+    owner "root"
+    group "root"
+    mode 0644
+    source "vhost.conf.erb"
+    variables(
+      :servername => vhost['servername'],
+      :serveralias => vhost['servieralias']
+    )
+  end
+
+  link "/etc/apache2/sites-enabled/#{confname}" do
+    to "/etc/apache2/sites-available/#{confname}"
+  end
 end
