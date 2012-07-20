ba9e56b0f25db19fba42968d66f2951d0c662a2f
Gabor Veszi <veszig@done.hu>
2012-07-20 11:40:37 +0200

    Adding some attributes.

---
+++ cookbooks/apache/attributes/apache.rb
@@ -0,0 +1,2 @@
+default['apache']['servername'] = "" # defaults to node['fqdn']
+default['apache']['serveradmin'] = "ops@example.com"
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -1,1 +1,14 @@
 package "apache2-mpm-worker"
+
+servername = node['apache']['servername'].to_s.empty? ? node['fqdn'] : node['apache']['servername']
+
+template "/etc/apache2/conf.d/identity" do
+  owner "root"
+  group "root"
+  mode 0644
+  source "identity.erb"
+  variables(
+    :servername => servername,
+    :serveradmin => node['apache']['serveradmin']
+  )
+end
